585c97ad98677926e1241e5108a470c4ca6bd78a
muye0503 <yanhaixiao2005@163.com>
2014-08-21 17:26:27 +0800

    Update replace_sql.py
    Add get hostname of the machine

--- replace_sql.py
+++ replace_sql.py
@@ -3,6 +3,19 @@
 import os,sys
 import re
 
+def GetHostname():
+    sys = os.name
+    
+    if sys == 'nt':
+        hostname = os.getenv('computername')
+        return hostname
+        print hostname
+    elif sys == 'posix':
+        hostname = os.getenv('HOSTNAME').split('-')[0]
+        return hostname
+    else:
+        return 'unknow hostname'
+        
 def Process_sql(src_sql,des_sql):
 
     head = """psql -h pglocalhost -U scncraft <<!eof
@@ -21,7 +34,7 @@ BEGIN;
             patt_body = re.compile(r'BEGIN|END')
             patt_tail = re.compile(r'^!eof')
             patt_sub = re.compile(r'SPVM\d{2,3}[A-Z]?')
-            repl = 'SPVM118A'
+            repl = GetHostname()         
             
             for line in f_src_sql:
                 if re.search(patt_head,line):
@@ -47,7 +60,7 @@ def Process_data(src_data,des_data):
             f_des_data = open(des_data,'w')
             
             patt_sub = re.compile(r'SPVM\d{2,3}[A-Z]?')
-            repl = 'SPVM118A'
+            repl = GetHostname()
             
             for line in f_src_data:
                 if re.search(patt_sub,line):
@@ -72,15 +85,17 @@ def Find_file(root_dir):
 if __name__ == '__main__': 
 
     cur_dir = os.path.dirname(os.path.abspath(sys.argv[0]))
-    input_path = os.path.join(cur_dir,'src_sql')
-    output_path = os.path.join(cur_dir,'des_sql')
+    input_path = os.path.join(cur_dir,'src')
+    output_path = os.path.join(cur_dir,'des')
 
     List_file = Find_file(input_path)
     for file in List_file:
         src_file = file
         des_file = os.path.join(output_path,os.path.basename(file))
         
+        print 'Process... %s' %file
         if os.path.basename(file).split('.')[-1] == 'sql': 
             Process_sql(src_file,des_file)
         elif os.path.basename(file).split('.')[-1] == 'data':
             Process_data(src_file,des_file)
+            
