26a4ecc82ae044474dde9e3d83099631ba19233d
muye0503 <yanhaixiao2005@163.com>
2015-11-26 07:28:28 -0800

    .

--- newsmth.py
+++ newsmth.py
@@ -7,11 +7,13 @@ from bs4 import BeautifulSoup
 url = "http://www.newsmth.net/nForum/board/PieLove"
 header = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.101 Safari/537.36'}
 r = requests.get(url, headers= header)
-bsObj = BeautifulSoup(r.text, 'html.parser')
+bsObj = BeautifulSoup(r.text, 'lxml')
 
-for item in bsObj.find('table', class_='board-list tiz').find_all('td', class_='title_9'):
-	print(item.string)
+content = bsObj.select(".title_9")
+for item in content[9:]:
+	#print(item)
 	print(item.a['href'])
+	print(item.a.get_text())
 	#print(type(item))
 	#print(item.attrs)
 
